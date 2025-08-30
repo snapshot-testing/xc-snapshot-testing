9a1f431e820353ba8e0c5ddcf8f21225f22093c7
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2025-08-30 07:48:25 -0300

    first commit

---
+++ Package.swift
@@ -0,0 +1,53 @@
+// swift-tools-version: 6.1
+
+import PackageDescription
+
+let package = Package(
+    name: "xc-snapshot-testing",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
+    products: [
+        .library(
+            name: "XCSnapshotTesting",
+            targets: ["XCSnapshotTesting"]
+        ),
+        .library(
+            name: "XCSnapshotTestingCustomDump",
+            targets: ["XCSnapshotTestingCustomDump"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.3"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", "601.0.0"..<"602.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "XCSnapshotTesting"
+        ),
+        .target(
+            name: "XCSnapshotTestingCustomDump",
+            dependencies: [
+                "XCSnapshotTesting",
+                .product(name: "CustomDump", package: "swift-custom-dump"),
+            ]
+        ),
+        .testTarget(
+            name: "XCSnapshotTestingTests",
+            dependencies: ["XCSnapshotTesting"],
+            exclude: [
+                "__Fixtures__",
+                "__Snapshots__",
+            ]
+        ),
+        .testTarget(
+            name: "XCSnapshotTestingCustomDumpTests",
+            dependencies: [
+                "XCSnapshotTestingCustomDump"
+            ]
+        ),
+    ]
+)
---
+++ Sources/XCSnapshotTestingCustomDump/CustomDump.swift
@@ -0,0 +1,27 @@
+import CustomDump
+import XCSnapshotTesting
+
+extension SyncSnapshot where Output == StringBytes {
+
+    /// A snapshot strategy for comparing any structure based on a
+    /// [custom dump](https://github.com/pointfreeco/swift-custom-dump).
+    ///
+    /// ```swift
+    /// try assert(of: user, as: .customDump)
+    /// ```
+    ///
+    /// Records:
+    ///
+    /// ```
+    /// User(
+    ///   bio: "Blobbed around the world.",
+    ///   id: 1,
+    ///   name: "Blobby"
+    /// )
+    /// ```
+    public static var customDump: SyncSnapshot<Input, Output> {
+        IdentitySyncSnapshot<StringBytes>.lines.pullback {
+            String(customDumping: $0)
+        }
+    }
+}
