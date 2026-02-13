0c07b42012c9b801446a2b78dc11198872b9e04b
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2026-02-13 18:12:00 -0300

    Updated swift-async-stream

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/o-nnerb/swift-async-stream", from: "1.3.1"),
+        .package(url: "https://github.com/o-nnerb/swift-async-stream", from: "1.3.2"),
         .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.4.1"),
     ],
     targets: [
